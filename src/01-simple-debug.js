4064ca1c9ed76f8f153645a7125b45b446e6bffe
Jerryomwesh <jerryomwesh@gmail.com>
2025-06-27 18:44:40 +0300

    updates src

--- src/01-simple-debug.js
+++ src/01-simple-debug.js
@@ -1,6 +1,14 @@
-console.log('Simple debugging example running.')
-debugger
+// console.log('Simple debugging example running.')
+// debugger
 
-let x = 99
-debugger
-console.log(x)
+// let x = 99
+// debugger
+// console.log(x)
+
+
+// Simple debugging example
+let x = 99;
+let y = 42;
+debugger; // Stops execution for debugging
+console.log("x:", x);
+console.log("y:", y);
--- src/02-advanced-debug.js
+++ src/02-advanced-debug.js
@@ -1,28 +1,51 @@
-console.log('Advanced debugging example running.')
-debugger
+// console.log('Advanced debugging example running.')
+// debugger
 
-// first, define the function.
+// // first, define the function.
+// function goodPractices() {
+//   let game = gameObject();
+//   for (let gameKey in game) {
+//     // are you ABSOLUTELY SURE what 'gameKey' is?
+//     // use the debugger to find out!
+//     debugger
+//     let teamObj = game[gameKey]
+//     for (let teamKey in teamObj) {
+//       // are you ABSOLUTELY SURE what 'teamKey' is?
+//       // use debugger to find out!
+//       debugger
+
+//       // what is 'data' at each loop through out this block?
+//       // when will the following line of code work and when will it break?
+//       let data = teamObj.player
+//       for (let key in data) {
+//         debugger
+//       }
+//     }
+//   }
+// }
+
+// // then, call the function so it runs!
+// goodPractices()
+
+
+
+// Function to debug deeply nested objects
 function goodPractices() {
-  let game = gameObject();
-  for (let gameKey in game) {
-    // are you ABSOLUTELY SURE what 'gameKey' is?
-    // use the debugger to find out!
-    debugger
-    let teamObj = game[gameKey]
-    for (let teamKey in teamObj) {
-      // are you ABSOLUTELY SURE what 'teamKey' is?
-      // use debugger to find out!
-      debugger
+    let game = gameObject();
+    for (let gameKey in game) {
+        debugger; // Check what gameKey is
+        let teamObj = game[gameKey];
 
-      // what is 'data' at each loop through out this block?
-      // when will the following line of code work and when will it break?
-      let data = teamObj.player
-      for (let key in data) {
-        debugger
-      }
+        for (let teamKey in teamObj) {
+            debugger; // Check what teamKey is
+            let data = teamObj.players; // Fixing typo (should be players, not player)
+
+            for (let key in data) {
+                debugger; // Check player data
+            }
+        }
     }
-  }
 }
 
-// then, call the function so it runs!
-goodPractices()
+// Call function to trigger debugging
+goodPractices();
